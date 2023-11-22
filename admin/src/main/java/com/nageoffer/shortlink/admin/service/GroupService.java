f833aeeef8e73f058840fd13498f8c3bb4323a12
Anyang Li <lianyang617@gmail.com>
2023-11-22 17:12:33 -0500

    feature: 开发短链接后管系统分组集合

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -2,6 +2,9 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
+
+import java.util.List;
 
 /**
  * 短链接分组接口层
@@ -13,4 +16,10 @@ public interface GroupService extends IService<GroupDO> {
      * @param groupName 分组名称
      */
     void saveGroup(String groupName);
+
+    /**
+     * 列出分组
+     * @return 分组列表
+     */
+    List<ShortLinkGroupRespDTO> listGroup();
 }

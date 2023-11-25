71c15136923b95633439b758c46d847444f092d6
Anyang Li <lianyang617@gmail.com>
2023-11-25 15:24:38 -0500

    短链接数据库配置变更

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/ShortLinkController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 短链接后管
+ */
 @RestController
 public class ShortLinkController {
     ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService(){};

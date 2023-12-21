708a4f79328631b3a85c1dec4506bba69da11fc3
Anyang Li <lianyang617@gmail.com>
2023-12-20 23:40:36 -0500

    今日统计

--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,8 +2,7 @@ package com.nageoffer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.nageoffer.shortlink.project.common.database.BaseDO;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import java.util.Date;
 
@@ -12,8 +11,11 @@ import java.util.Date;
  * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 @EqualsAndHashCode(callSuper = true)
-@Data
 @TableName("t_link_stats_today")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
         /**
          * ID
